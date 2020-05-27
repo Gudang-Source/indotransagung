3ff6d99d2007b5a9333eb7fbfb135c8d97b3ebd4
imgan <imamsatrianta@gmail.com>
2020-05-27 21:21:57 +0700

    initial commit

---
+++ application/views/landing.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Indotrans Agung Putra | Under Maintenance</title>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&family=Damion&display=swap" rel="stylesheet">
+    <style>
+        body{
+            background: white url("<?php echo base_url(); ?>asset/images/wrench.png") no-repeat fixed;
+            background-position: top right;
+        }
+        .container{
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            height: 100vh;
+        }
+        .logo{
+            position: absolute;
+            top: 50px;
+            left: 50px;
+        }
+        .container h1{
+            font-family: 'Damion', cursive;
+            font-size: 100px;
+            font-weight: 400;
+            margin-bottom: 10px;
+            color: #333;
+            text-align: center;
+        }
+        .container h4{
+            font-family: 'Poppins', sans-serif;
+            margin-top: 0px;
+            color: #999;
+            font-size: 20px;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <img class="logo" alt="Logo" src='<?php echo base_url(); ?>asset/front/img/logo.jpeg' width='50' height='50'>
+        <h1>Coming Soon</h1>
+        <h4>Website is under maintenance. We will back at to You soon.</h4>
+    </div>
+</body>
+</html>
